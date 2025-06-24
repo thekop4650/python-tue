2c2ccad5832d662b8ea043c97fa70e3956acd136
thekop4650 <thekoprov2007@gmail.com>
2025-06-24 16:50:03 +0700

    Commit number

--- Lecture-01/testprint.py
+++ Lecture-01/testprint.py
@@ -4,5 +4,3 @@ print("I'm good and\"I will do my best\"" )
 print('/\\')
 print('\ta\na\ta\ta\n\ta')
 
- git config --global user.email "thekoprov2007@gmail.com"
-  git config --global user.name "thekop4650"
---
+++ Lecture-01/testprintnumber.py
@@ -0,0 +1,8 @@
+from math import pi 
+
+print('%d'%100)
+print("%d" %100.58)
+print("%f" %-100.69)
+print("%.2f" %100.658)
+print("My age is"  + str(25) + " I have" + str(3500.50) + " Bth.")
+print("5+4=",5+4)
