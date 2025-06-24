4ffc0d2bc66774a109d85f498b168736de02caae
thekop4650 <thekoprov2007@gmail.com>
2025-06-24 16:18:39 +0700

    MyFistCommit

---
+++ Lecture-01/testprint.py
@@ -0,0 +1,8 @@
+print('Hello world')
+print("I'm Hungry")
+print("I'm good and\"I will do my best\"" )
+print('/\\')
+print('\ta\na\ta\ta\n\ta')
+
+ git config --global user.email "thekoprov2007@gmail.com"
+  git config --global user.name "thekop4650"
